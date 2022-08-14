bd41dcee8a17687c7364d24395bb852d747372b4
Ozenua Oluwatobi John <30311064+OTANatio@users.noreply.github.com>
2022-08-14 12:25:43 +0100

    First commit

---
+++ FILES/image/index.js
@@ -0,0 +1,39 @@
+ 
+// logTittle =("my java") 
+let name  = "Oluwaseun";
+let Surname = "Akeredolu";
+let age = 19;
+let future = 10;
+let ageSum = age + future;
+
+let person = {
+  name : 'oluwaseun',
+  age : 19,
+  state : 'illorin',
+}
+
+function greet (name,lastname) {
+    console.log('hello ' +  name + ' ' +lastname );
+}
+
+greet('oluwaseun', 'isreal');
+
+let selectedcolors= ['red',  'blue', 'yellow']
+
+console.log('My name is ' + person.name + " and I am " + person.age + "years old. My favorite color is " + selectedcolors[2]);
+
+//  let name =" tobiloba";
+
+var place = "lagos"; 
+var places = ["lagos", "illorin", "ado"]; 
+// log (place);
+
+// var persons =[
+//     { name: "seun", age:"19" },
+//     { name: "moyin", age:"17" }
+// ]
+// for (var i= 0; i <=  person.length; i++ ) {
+//     log(person[i], name );
+//     log(person[i], name);
+//     log("------------")
+// }
---
+++ FILES/image/loop.js
@@ -0,0 +1,8 @@
+
+let a = 5;
+let b = 3;
+let c = 2;
+
+console.log(a + b + c - b)
+
+  
